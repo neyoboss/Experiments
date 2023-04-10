9cc699e0dfcb41340d39707fbda944e1faf347d0
neyoboss <neykneyk1@gmail.com>
2023-04-10 20:51:05 +0200

    Added empty test for CI/CD setup

---
+++ back-end/test/MatchServiceTest/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace MatchServiceTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ back-end/test/ProfileServiceTest/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace ProfileServiceTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
