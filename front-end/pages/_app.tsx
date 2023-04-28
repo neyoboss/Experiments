66892ccbc7b1e0d751d54e29ac831376fe9e23db
Neyko Neykov <neyko@Backspace.local>
2023-04-28 14:34:48 +0200

    Added login front-end

--- front-end/pages/_app.tsx
+++ front-end/pages/_app.tsx
@@ -7,4 +7,5 @@ export default function App({ Component, pageProps }: AppProps) {
     <UserProvider>
       <Component {...pageProps} />
     </UserProvider>
-  )};
+  )
+};
--- front-end/pages/index.tsx
+++ front-end/pages/index.tsx
@@ -1,14 +1,31 @@
 import { Inter } from 'next/font/google'
 import Link from 'next/link'
+import { useUser } from '@auth0/nextjs-auth0/client';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return (
-    <>
-      <Link href="/api/auth/login">
-        <button>Login</button>
-      </Link>
-    </>
-  )
+  // const { user, isLoading, error } = useUser();
+
+  // if (isLoading) {
+  //   return <div>Loading...</div>;
+  // }
+
+  // if (error) {
+  //   return <div>Error: {error.message}</div>;
+  // }
+
+  // return (
+  //   <div>
+  //     {user ? (
+  //       <div>
+  //         <h1>Welcome, {user.name}</h1>
+  //         <button onClick={() => fetch('/api/auth/logout')}>Log Out</button>
+  //       </div>
+  //     ) : (
+  //       <button onClick={() => fetch('/api/auth/login')}>Log In</button>
+  //     )}
+  //   </div>
+  // );
+  return <a href="/api/auth/login">Login</a>;
 }
