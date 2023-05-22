63de0300d518c4a7426bd48c85bfda03d1d711d3
Neykov,Neyko N <n.neykov@student.fontys.nl>
2023-05-22 12:36:44 +0200

    front end

--- front-end/middleware.ts
+++ front-end/middleware.ts
@@ -1,17 +1,17 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+// import { NextResponse } from 'next/server';
+// import type { NextRequest } from 'next/server';
 
-// This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-    // if (request.nextUrl.pathname.startsWith('/secure')) {
-    //     const { cookies } = request;
-    //     if (!cookies.get('access_token')) {
-    //         const response = NextResponse.redirect(new URL('/login', request.url));
-    //         return response;
-    //     }
-    // }
+// // This function can be marked `async` if using `await` inside
+// export function middleware(request: NextRequest) {
+//     // if (request.nextUrl.pathname.startsWith('/secure')) {
+//     //     const { cookies } = request;
+//     //     if (!cookies.get('access_token')) {
+//     //         const response = NextResponse.redirect(new URL('/login', request.url));
+//     //         return response;
+//     //     }
+//     // }
 
-    // const response = NextResponse.next();
-    // return response;
+//     // const response = NextResponse.next();
+//     // return response;
 
-}
+// }
--- front-end/pages/userContext.tsx
+++ front-end/pages/userContext.tsx
@@ -2,21 +2,29 @@ import { ReactNode, createContext, useContext, useState } from 'react';
 
 
 interface IUserContext {
-    user: any;
-    setUser: (user: any) => void;
-  }
-export const UserContext = createContext<IUserContext|undefined>(undefined);
+  user: any;
+  setUser: (user: any) => void;
+}
+export const UserContext = createContext<IUserContext | undefined>(undefined);
 
-interface UserProviderProps{
-    children:ReactNode;
+interface UserProviderProps {
+  children: ReactNode;
 }
 
 export const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
-    const [user, setUser] = useState<any>(null);
-  
-    return (
-      <UserContext.Provider value={{ user, setUser }}>
-        {children}
-      </UserContext.Provider>
-    );
-  };
+  const [user, setUser] = useState<any>(null);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export default function nothinf() {
+  return (
+    <>
+      <h1>AAAAA</h1>
+    </>
+  )
+}
